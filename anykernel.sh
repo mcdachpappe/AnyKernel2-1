5b9a6f6d190cbffb1045c8ea8ac030b615150fdb
danny <noreference@web.de>
2018-10-11 20:51:17 +0200

    anykernel: welcome mcd-kernel
    * anykernel: add a simple software check before installing
    * anykernel: add our own ramdisk
    * banner: update to the new kernel name

--- anykernel.sh
+++ anykernel.sh
@@ -3,23 +3,23 @@
 
 ## AnyKernel setup
 # begin properties
-properties() {
-kernel.string=RenderZenith kernel for OP3 modified by mcd
+properties() { '
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=OnePlus3
-device.name2=oneplus3
-device.name3=OnePlus3T
-device.name4=oneplus3t
-} # end properties
+do.cleanuponabort=1
+device.name1=OnePlus 3T
+device.name2=OnePlus3T
+device.name3=oneplus3t
+device.name4=OnePlus3
+device.name5=oneplus3
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -29,13 +29,38 @@ ramdisk_compression=auto;
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
 chown -R root:root $ramdisk/*;
+
+
+## Alert of unsupported OxygenOS / Android version
+oos_ver=$(file_getprop /system/build.prop ro.build.ota.versionname)
+if [ -z $oos_ver ]; then
+    ui_print " "
+    ui_print "   Only OxygenOS is supported!"
+    ui_print "Aborting..."
+    ui_print " "
+    exit 9
+fi
+android_ver=$(file_getprop /system/build.prop ro.build.version.release)
+if [ $android_ver != "8.0.0" ]; then
+    ui_print " "
+    ui_print "   Only OxygenOS-Oreo (8.0.0) is supported!"
+    ui_print "Aborting..."
+    ui_print " "
+    exit 9
+fi
+
 
 ## AnyKernel install
 dump_boot;
 
+
 # begin ramdisk changes
-insert_line init.rc "init.renderzenith.rc" after "import /init.environ.rc" "import /init.renderzenith.rc\n";
+
+# Import rz-mcd.rc
+remove_line init.rc "init.rz-mcd.rc";
+insert_line init.rc "init.rz-mcd.rc" after "import /init.environ.rc" "import /init.rz-mcd.rc";
 
 # sepolicy
 $bin/magiskpolicy --load sepolicy --save sepolicy \

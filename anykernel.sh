6d78ea1c6a7bd6d55aae95ce2f9e7e2a02842bc1
danny aeng <noreference@web.de>
2019-03-23 17:17:50 +0100

    AnyKernel2: Initial Custom ROM [OREO | PIE] bringup
    * do not support oxygenos
    * check current init.qcom.rc and inject init.mcd.rc accordingly
    * import mcd-power.sh
    * import custom perfboostconfig.xml
    * import sepolicy patches
    
    Signed-off-by: danny aeng <noreference@web.de>

--- anykernel.sh
+++ anykernel.sh
@@ -4,24 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
+do.cleanuponabort=1
+device.name1=OnePlus3
+device.name2=oneplus3
+device.name3=OnePlus3T
+device.name4=oneplus3t
 device.name5=
-supported.versions=
+supported.versions=8.1.0 - 9
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -34,30 +33,91 @@ chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
 
+## Alert of unsupported OOS
+oos_ver=$(file_getprop /system/build.prop ro.build.ota.versionname)
+if [ "$oos_ver" != "" ]; then
+    ui_print "  This kernel version was not designed for";
+    ui_print "  OxygenOS. Please select the OxygenOS";
+    ui_print "  version of this kernel to proceed.";
+    exit 9
+fi
+
+
+## start system changes
+mount -o rw,remount -t auto /system >/dev/null;
+
+
+# insert custom inits
+if [ -f /system/vendor/etc/init/hw/init.qcom.rc ]; then
+    ui_print " ";
+    ui_print "  Injecting in /vendor/etc/init/hw/init.qcom.rc...";
+    # import mcd.rc
+    cp /tmp/anykernel/ramdisk/init.mcd.rc /system/vendor/etc/init/hw/init.mcd.rc;
+    insert_line /system/vendor/etc/init/hw/init.qcom.rc "init.mcd.rc" after "import /vendor/etc/init/hw/init.qcom.usb.rc" "import /vendor/etc/init/hw/init.mcd.rc";
+    # chmod
+    chmod 644 /system/vendor/etc/init/hw/init.mcd.rc;
+fi
+## end system changes
+
+
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+## begin ramdisk changes
+if [ ! -f /system/vendor/etc/init/hw/init.qcom.rc ]; then
+    ui_print " ";
+    ui_print "  Injecting in /init.rc...";
+    insert_line /init.rc "init.mcd.rc" after "import /init.usb.configfs.rc" "import /init.mcd.rc";
+fi
+## end ramdisk changes
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+## sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+    "allow { audioserver system_server location sensors } diag_device chr_file { read write }" \
+    "allow { init modprobe } rootfs system module_load" \
+    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } file { getattr read open }" \
+    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } dir { getattr read open }" \
+    "allow init { system_file vendor_file vendor_configs_file } file mounton" \
+    "allow init rootfs file execute_no_trans" \
+    "allow hal_perf_default hal_perf_default capability { kill }" \
+    "allow hal_perf_default hal_graphics_composer_default process { signull }" \
+    "allow hal_perf_default kernel dir { read search open }" \
+    "allow hal_iop_default priv_app dir { search }" \
+    "allow hal_memtrack_default qti_debugfs file { read open getattr }" \
+    "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+    "allow healthd proc_stat file { read open getattr }" \
+    "allow healthd healthd capability { sys_ptrace }" \
+    "allow dumpstate fuse dir search" \
+    "allow dumpstate fuse file getattr" \
+    "allow dumpstate theme_data_file file { read open getattr }" \
+    "allow priv_app { cache_private_backup_file unlabeled } dir getattr" \
+    "allow shell dalvikcache_data_file dir write" \
+    "allow untrusted_app proc_stat file { read open getattr }" \
+    "allow untrusted_app rootfs file { read open getattr }" \
+    "allow untrusted_app atrace_exec file { read open getattr }" \
+    "allow untrusted_app audioserver_exec file { read open getattr }" \
+    "allow untrusted_app blkid_exec file { read open getattr }" \
+    "allow untrusted_app bootanim_exec file { read open getattr }" \
+    "allow untrusted_app bootstat_exec file { read open getattr }" \
+    "allow untrusted_app cameraserver_exec file { read open getattr }" \
+    "allow untrusted_app cgroup dir { read open getattr }" \
+    "allow untrusted_app qti_debugfs dir { search }" \
+    "allow untrusted_app hal_memtrack_hwservice hwservice_manager { find }" \
+    "allow untrusted_app sysfs_kgsl dir { read write getattr open }" \
+    "allow untrusted_app sysfs_kgsl file { read write getattr open }" \
+    "allow untrusted_app sysfs dir { read write getattr open }" \
+    "allow untrusted_app sysfs file { read write getattr open }" \
+    "allow untrusted_app sysfs_leds dir search" \
+    "allow untrusted_app sysfs_leds lnk_file read" \
+    "allow untrusted_app sysfs_zram dir search" \
+    "allow untrusted_app sysfs_zram file { read open getattr }" \
+    "allow vold logd dir read" \
+    "allow vold logd lnk_file getattr" \
+    ;
 
-# end ramdisk changes
 
 write_boot;
 
 ## end install
-
---
+++ ramdisk/init.mcd-power.sh
@@ -0,0 +1,209 @@
+#!/system/bin/sh
+
+################################################################################
+# helper functions to allow Android init like script
+
+function write() {
+    echo -n $2 > $1
+}
+
+function copy() {
+    cat $1 > $2
+}
+
+# macro to write pids to system-background cpuset
+function writepid_sbg() {
+    until [ ! "$1" ]; do
+        echo -n $1 > /dev/cpuset/system-background/tasks;
+        shift;
+    done;
+}
+
+function writepid_top_app() {
+    until [ ! "$1" ]; do
+        echo -n $1 > /dev/cpuset/top-app/tasks;
+        shift;
+    done;
+}
+################################################################################
+
+{
+
+sleep 5
+
+# cpu
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+chmod 0664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chmod 0664 /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+chmod 0664 /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+chmod 0664 /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+
+chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+chown system system /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chown system system /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+chown system system /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+chown system system /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+
+# display kcal calibration
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_cont
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_hue
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_sat
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_val
+
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_cont
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_hue
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_sat
+chmod 0664 /sys/devices/platform/kcal_ctrl.0/kcal_val
+
+# Disable sched_boost
+echo "0" > /proc/sys/kernel/sched_boost
+
+# Set default schedTune value for foreground/top-app
+echo "0" > /dev/stune/top-app/schedtune.boost
+
+# Dynamic Stune Boost during sched_boost
+echo "10" > /dev/stune/top-app/schedtune.sched_boost
+
+# Stune configuration
+echo "10" > /sys/module/cpu_boost/parameters/dynamic_stune_boost
+echo "300" > /sys/module/cpu_boost/parameters/dynamic_stune_boost_ms
+
+# Set default schedTune value for foreground/top-app
+echo "1" > /dev/stune/foreground/schedtune.prefer_idle
+echo "1" > /dev/stune/top-app/schedtune.prefer_idle
+
+# Disable thermal hotplug to switch governor
+echo "0" > /sys/module/msm_thermal/core_control/enabled
+
+# Bring back main cores CPU 0,2
+echo "1" > /sys/devices/system/cpu/cpu0/online
+echo "1" > /sys/devices/system/cpu/cpu2/online
+
+# Configure governor settings for little cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "1000" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+echo "10000" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+echo "0" > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable
+
+# Configure governor settings for big cluster
+echo "schedutil" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "1000" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/up_rate_limit_us
+echo "10000" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/down_rate_limit_us
+echo "0" > /sys/devices/system/cpu/cpu2/cpufreq/schedutil/iowait_boost_enable
+
+# Re-enable thermal hotplug
+echo "1" /sys/module/msm_thermal/core_control/enabled
+
+# CPUFreq control
+echo "307200" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "1593600" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "307200" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo "2342400" > sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+
+# Touchboost configuration
+echo "0" > /sys/module/msm_performance/parameters/touchboost
+
+# Input boost configuration
+echo "1" > /sys/module/cpu_boost/parameters/input_boost_enabled
+echo "0:844800 2:691200" > /sys/module/cpu_boost/parameters/input_boost_freq
+echo "80" > /sys/module/cpu_boost/parameters/input_boost_ms
+
+# Set thermal restrictions
+echo "0" > /sys/kernel/msm_thermal/enabled
+echo "1516800 2246400 41 40" > /sys/kernel/msm_thermal/zone0
+echo "1440000 2150400 42 41" > /sys/kernel/msm_thermal/zone1
+echo "1363200 2054400 43 42" > /sys/kernel/msm_thermal/zone2
+echo "1363200 1977600 44 43" > /sys/kernel/msm_thermal/zone3
+echo "1286400 1900800 45 44" > /sys/kernel/msm_thermal/zone4
+echo "1286400 1824000 47 45" > /sys/kernel/msm_thermal/zone5
+echo "1132800 1670400 49 47" > /sys/kernel/msm_thermal/zone6
+echo "1056000 1363200 54 49" > /sys/kernel/msm_thermal/zone7
+echo "902400 1056000 58 54" > /sys/kernel/msm_thermal/zone8
+echo "844800 902400 60 58" > /sys/kernel/msm_thermal/zone9
+echo "768000 748800 63 60" > /sys/kernel/msm_thermal/zone10
+echo "4000" > /sys/kernel/msm_thermal/sampling_ms
+echo "1" > /sys/kernel/msm_thermal/enabled
+
+# Configure governor for devfreq/kgsl
+echo "bw_hwmon" > /sys/class/devfreq/soc:qcom,cpubw/governor
+echo "msm-adreno-tz" > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+
+# Set I/O scheduler
+setprop sys.io.scheduler "maple"
+
+# Tweak IO performance after boot complete
+echo "1" > /sys/block/sda/queue/iostats
+echo "1" > /sys/block/sde/queue/iostats
+echo "maple" > /sys/block/sda/queue/scheduler
+echo "maple" > /sys/block/sde/queue/scheduler
+echo "maple" > /sys/block/dm-0/queue/scheduler
+echo "maple" > /sys/block/dm-1/queue/scheduler
+echo "128" > /sys/block/sda/queue/nr_requests
+echo "128" > /sys/block/sde/queue/nr_requests
+echo "256" > /sys/block/sda/queue/read_ahead_kb
+echo "256" > /sys/block/sde/queue/read_ahead_kb
+echo "256" > /sys/block/dm-0/queue/read_ahead_kb
+echo "256" > /sys/block/dm-1/queue/read_ahead_kb
+
+# Disable Serial Console
+echo "N" > /sys/module/printk/parameters/console_suspend
+
+# Set sync wakee policy tunable
+echo "1" > /proc/sys/kernel/sched_prefer_sync_wakee_to_waker
+
+# according to Qcom this legacy value improves first launch latencies
+# stock value is 512k
+setprop dalvik.vm.heapminfree 2m
+
+## write pids to system-background cpuset
+
+sleep 20
+
+QSEECOMD=`pidof qseecomd`
+THERMAL-ENGINE=`pidof thermal-engine`
+TIME_DAEMON=`pidof time_daemon`
+IMSQMIDAEMON=`pidof imsqmidaemon`
+IMSDATADAEMON=`pidof imsdatadaemon`
+DASHD=`pidof dashd`
+CND=`pidof cnd`
+DPMD=`pidof dpmd`
+RMT_STORAGE=`pidof rmt_storage`
+TFTP_SERVER=`pidof tftp_server`
+NETMGRD=`pidof netmgrd`
+IPACM=`pidof ipacm`
+QTI=`pidof qti`
+LOC_LAUNCHER=`pidof loc_launcher`
+QSEEPROXYDAEMON=`pidof qseeproxydaemon`
+IFAADAEMON=`pidof ifaadaemon`
+LOGCAT=`pidof logcat`
+LMKD=`pidof lmkd`
+
+writepid_sbg $QSEECOMD
+writepid_sbg $THERMAL-ENGINE
+writepid_sbg $TIME_DAEMON
+writepid_sbg $IMSQMIDAEMON
+writepid_sbg $IMSDATADAEMON
+writepid_sbg $DASHD
+writepid_sbg $CND
+writepid_sbg $DPMD
+writepid_sbg $RMT_STORAGE
+writepid_sbg $TFTP_SERVER
+writepid_sbg $NETMGRD
+writepid_sbg $IPACM
+writepid_sbg $QTI
+writepid_sbg $LOC_LAUNCHER
+writepid_sbg $QSEEPROXYDAEMON
+writepid_sbg $IFAADAEMON
+writepid_sbg $LOGCAT
+writepid_sbg $LMKD
+
+## end write pids to system-background cpuset
+
+echo "mcd-power.sh executed" > /dev/kmsg
+
+}&
+

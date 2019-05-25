9d6fa14e4a4db60d3de075dda78d5e24a6009fbb
danny aeng <noreference@web.de>
2019-05-25 19:26:59 +0200

    AK2: initial mcd kernel bring up
    * Hexpatch the kernel with magiskboot
    adapted from https://github.com/0ctobot/AnyKernel2/commit/5af79de42902c91df36e92a07b06e7045ae35842
    
    * Systemlessly apply custom settings
    adapted from https://github.com/0ctobot/AnyKernel2/commit/243d0de29545c190d7d8492c5fbe286d8e45e18f
    
    * add spectrum support and fku profiles support
    
    Thanks to @0ctobot for this solution!
    
    Signed-off-by: danny aeng <noreference@web.de>

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
-device.name5=
-supported.versions=
+device.name1=OnePlus6
+device.name2=OnePlus6T
+device.name3=oneplus6
+device.name4=oneplus6t
+supported.versions=9
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -34,30 +33,60 @@ chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
 
+# Systemlessly apply custom settings
+mountpoint -q /data && {
+  # Add custom files
+  mkdir -p /data/adb/magisk_simple/vendor/etc;
+  cp /tmp/anykernel/patch/msm_irqbalance.conf /data/adb/magisk_simple/vendor/etc;
+  cp /tmp/anykernel/patch/init.spectrum.rc /data/adb/magisk_simple;
+  cp /tmp/anykernel/patch/init.performance_profiles.rc /data/adb/magisk_simple;
+  chmod 644 /data/adb/magisk_simple/vendor/etc/msm_irqbalance.conf;
+  chmod 750 /data/adb/magisk_simple/init.spectrum.rc;
+  chmod 750 /data/adb/magisk_simple/init.performance_profiles.rc;
+
+  # Add second-stage late init scripts
+  mkdir -p /data/adb/service.d;
+  cp /tmp/anykernel/patch/96-mcd.sh /data/adb/service.d;
+  cp /tmp/anykernel/patch/96-mcd-power.sh /data/adb/service.d;
+  chmod 755 /data/adb/service.d/96-mcd.sh;
+  chmod 755 /data/adb/service.d/96-mcd-power.sh;
+}
+
+
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; 
+    ui_print "  Magisk detected! Patching cmdline so";
+    ui_print "  reflashing Magisk is not necessary...";
+    ui_print " ";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi;
+
+
+# Clean up other kernels' ramdisk overlay files
+rm -rf $ramdisk/overlay;
+
+
+# Install the boot image
 write_boot;
 
 ## end install
-

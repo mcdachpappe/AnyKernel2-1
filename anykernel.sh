faba131d130a362fc2589e2184e9f91f127e4f88
danny <noreference@web.de>
2019-05-28 11:45:05 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -9,16 +9,16 @@ do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus6
-device.name2=OnePlus6T
-device.name3=oneplus6
-device.name4=oneplus6t
-supported.versions=9
+device.name1=OnePlus3
+device.name2=oneplus3
+device.name3=oneplus3t
+device.name4=OnePlus3T
+supported.versions=8.1.0,9
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 
@@ -26,37 +26,41 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
 
-# Define target paths
-mount -o rw,remount -t auto /vendor > /dev/null;
-target="/vendor/etc/init/hw/init.target.rc";
-irq="/vendor/etc/msm_irqbalance.conf";
-
-# Cleanup previous performance additions
-for files in "${target}" "${irq}"
-do
-  restore_file $files;
-done
+## Alert of unsupported OOS
+oos_ver=$(file_getprop /system/build.prop ro.build.ota.versionname);
+if [ $oos_ver ]; then
+    ui_print " ";
+    ui_print "  Warning: incompatible ROM detected.";
+    ui_print " ";
+    ui_print "  This Kernel version does not support";
+    ui_print "  OxygenOS. Please select the OxygenOS";
+    ui_print "  version of this Kernel to proceed.";
+    ui_print " ";
+    ui_print "  - Installer will abort now.";
+    exit 0;
+fi;
 
 
 # Systemlessly apply custom settings
 mountpoint -q /data && {
-  # Install custom IRQ Balance config
-  mkdir -p /data/adb/magisk_simple/vendor/etc;
-  cp /tmp/anykernel/msm_irqbalance.conf /data/adb/magisk_simple/vendor/etc;
+    # Install custom IRQ Balance config
+    mkdir -p /data/adb/magisk_simple/system/vendor/etc/perf;
+    cp /tmp/anykernel/perfboostconfig.xml /data/adb/magisk_simple/system/vendor/etc/perf/perfboostconfig.xml;
+    chmod 0644 /data/adb/magisk_simple/system/vendor/etc/perf/perfboostconfig.xml;
 
-  # Install second-stage late init scripts
-  mkdir -p /data/adb/service.d;
-  cp /tmp/anykernel/96-mcd.sh /data/adb/service.d;
-  cp /tmp/anykernel/96-mcd-power.sh /data/adb/service.d;
-  chmod +x /data/adb/service.d/96-mcd.sh;
-  chmod +x /data/adb/service.d/96-mcd-power.sh;
+    # Install second-stage late init scripts
+    mkdir -p /data/adb/service.d;
+    cp /tmp/anykernel/96-mcd.sh /data/adb/service.d;
+    cp /tmp/anykernel/96-mcd-power.sh /data/adb/service.d;
+    cp /tmp/anykernel/96-mcd-sepolicy.sh /data/adb/service.d;
+    chmod +x /data/adb/service.d/96-mcd.sh;
+    chmod +x /data/adb/service.d/96-mcd-power.sh;
 }
 
 
@@ -64,28 +68,18 @@ mountpoint -q /data && {
 dump_boot;
 
 
-# If the kernel image and dtbs are separated in the zip
-decompressed_image=/tmp/anykernel/kernel/Image
-compressed_image=$decompressed_image.gz
-if [ -f $compressed_image ]; then
-  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
-  if [ -d $ramdisk/.backup ]; then
-    ui_print " "; 
-    ui_print "  Magisk detected! Patching cmdline so";
-    ui_print "  reflashing Magisk is not necessary...";
-    ui_print " ";
-    $bin/magiskboot --decompress $compressed_image $decompressed_image;
-    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
-    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
-  fi;
-
-  # Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
-fi;
+# sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+    "allow init rootfs file execute_no_trans" \
+    "allow { init modprobe } rootfs system module_load" \
+    "allow init { system_file vendor_file vendor_configs_file } file mounton" \
+    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } file { getattr read open } " \
+    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } dir { getattr read open } " \
+    ;
 
 
-# Clean up other kernels' ramdisk overlay files
-rm -rf $ramdisk/overlay;
+# Clean up other kernels' ramdisk files
+rm -rf $ramdisk;
 
 
 # Install the boot image

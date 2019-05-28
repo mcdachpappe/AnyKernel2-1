fb33c4eec78470eeac8347c98fc046febb481be1
danny aeng <noreference@web.de>
2019-05-28 18:02:38 +0200

    AK2: rewrite this mess
    why complicated if it's so easy?
    
    * sync tools from osm0sis/tools
    
    Signed-off-by: danny aeng <noreference@web.de>

--- anykernel.sh
+++ anykernel.sh
@@ -2,13 +2,13 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# EDIFY properties
+# begin properties
 properties() { '
 kernel.string=
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=OnePlus3
 device.name2=oneplus3
 device.name3=oneplus3t
@@ -17,23 +17,24 @@ supported.versions=8.1.0,9
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/624000.ufshc/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-## end setup
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
+
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
+
 ## Alert of unsupported OOS
-oos_ver=$(file_getprop /system/build.prop ro.build.ota.versionname)
+oos_ver=$(file_getprop /system/build.prop ro.build.ota.versionname);
 if [ $oos_ver ]; then
     ui_print " ";
     ui_print "  Warning: incompatible ROM detected.";
@@ -43,81 +44,13 @@ if [ $oos_ver ]; then
     ui_print "  version of this Kernel to proceed.";
     ui_print " ";
     ui_print "  - Installer will abort now.";
-    exit 0
+    exit 0;
 fi;
 
-## start system changes
-mount -o remount,rw /system;
-
-## Alert of insufficient /system space
-avail_space=`df -kh /system | grep -v "Filesystem" | awk '{ print $5 }' | cut -d'%' -f1`
-if [ "$avail_space" == "100" ]; then
-    ui_print " ";
-    ui_print "  Warning: your /system partition is full.";
-    ui_print " ";
-    ui_print "  This Kernel needs at least 10 MB free space";
-    ui_print "  on your /system partition."
-    ui_print " ";
-    ui_print "  Do you want to delete 'G-Play Movies' now?";
-    ui_print " ";
-    ui_print "  Press: Volume Up [YES] || Volume Down [NO]";
-    # keycheck to delete system-app
-    /tmp/anykernel/tools/keycheck; KVAR=$?
-    if [ $KVAR -eq 41 ]; then
-        ui_print " ";
-        ui_print "  - Installer will abort now.";
-        exit 0
-    elif [ $KVAR -eq 42 ]; then
-        ui_print " ";
-        ui_print "  - Deleting Google Play Movies...";
-        rm -rf /system/app/Videos;
-        rm -rf /data/data/com.google.android.videos;
-        rm -f /data/dalvik-cache/*/*Videos.apk* ;
-    fi;
-fi;
-
-# insert custom inits
-if [ -f /system/vendor/etc/init/hw/init.qcom.rc ]; then
-    ui_print " ";
-    ui_print "  - Injecting in /vendor/etc/init/hw/init.qcom.rc";
-    # import mcd.rc
-    cp /tmp/anykernel/ramdisk/init.mcd.rc /system/vendor/etc/init/hw/init.mcd.rc;
-    insert_line /system/vendor/etc/init/hw/init.qcom.rc "init.mcd.rc" after "import /vendor/etc/init/hw/init.qcom.usb.rc" "import /vendor/etc/init/hw/init.mcd.rc";
-    # import spectrum.rc
-    cp /tmp/anykernel/ramdisk/init.spectrum.rc /system/vendor/etc/init/hw/init.spectrum.rc;
-    replace_line /system/vendor/etc/init/hw/init.mcd.rc "import /init.spectrum.rc" "import /vendor/etc/init/hw/init.spectrum.rc";
-    remove_line /system/vendor/etc/init/hw/init.qcom.rc "import /vendor/etc/init/hw/init.spectrum.rc";
-    # chmod
-    chmod 644 /system/vendor/etc/init/hw/init.mcd.rc;
-    chmod 644 /system/vendor/etc/init/hw/init.spectrum.rc;
-fi;
-
-mount -o remount,ro /system;
-
-## end system changes
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
 
-if [ ! -f /system/vendor/etc/init/hw/init.qcom.rc ]; then
-    ui_print " ";
-    ui_print "  - Injecting in /init.rc";
-    insert_line /init.rc "init.mcd.rc" after "import /init.usb.configfs.rc" "import /init.mcd.rc";
-fi;
-
-# sepolicy
-$bin/magiskpolicy --load sepolicy --save sepolicy \
-    "allow init rootfs file execute_no_trans" \
-    "allow { init modprobe } rootfs system module_load" \
-    "allow init { system_file vendor_file vendor_configs_file } file mounton" \
-    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } file { getattr read open } " \
-    "allow { msm_irqbalanced hal_perf_default } { rootfs kernel } dir { getattr read open } " \
-    ;
-
-# end ramdisk changes
-
+# Install the boot image
 write_boot;
-
-## end install
